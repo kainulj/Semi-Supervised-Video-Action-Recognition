9f208a25b5c8c361901bfb1e76a666e513be83fe
Jaakko Kainulainen <jaakko.kainulainen@aalto.fi>
2023-04-19 19:18:20 +0300

    Update train_ssl.sh

--- train_ssl.sh
+++ train_ssl.sh
@@ -9,9 +9,9 @@ cp $WRKDIR/train.tar /tmp/$SLURM_JOB_ID           # copy tarred input files
 cp $WRKDIR/validation.tar /tmp/$SLURM_JOB_ID           # copy tarred input files
 cp $WRKDIR/train.txt /tmp/$SLURM_JOB_ID           # copy tarred input files
 cp $WRKDIR/val.txt /tmp/$SLURM_JOB_ID           # copy tarred input files
-cp -R $WRKDIR/supervised /tmp/$SLURM_JOB_ID
-cp $WRKDIR/models.py tmp/$SLURM_JOB_ID/supervised  # Copy the models file to the supervised folder
-cp $WRKDIR/ViT-B_16.npztmp/$SLURM_JOB_ID/supervised # Copy the pretrained weights to the supervised folder
+cp -R $WRKDIR/FixMatch  /tmp/$SLURM_JOB_ID
+cp $WRKDIR/models.py tmp/$SLURM_JOB_ID/FixMatch  # Copy the models file to the supervised folder
+cp $WRKDIR/ViT-B_16.npz tmp/$SLURM_JOB_ID/FixMatch  # Copy the pretrained weights to the supervised folder
 cd /tmp/$SLURM_JOB_ID
 
 trap "rm -rf /tmp/$SLURM_JOB_ID; exit" TERM EXIT  # set the trap: when killed or exits abnormally you clean up your stuff
@@ -21,7 +21,7 @@ tar xf train.tar                                  # untar the files
 echo "tar validation set"
 tar xf validation.tar                                  # untar the files
 echo "run the python script"
-cd supervised 
+cd FixMatch 
 srun  python3 train.py --model 2 --batch_size 1 --epochs 40 --threshold 0.25   # train the model
 mv  'FactorizedEncoder.pth' $WRKDIR
 
